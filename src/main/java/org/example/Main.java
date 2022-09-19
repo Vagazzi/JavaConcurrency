fa2a034d2fec12d5c140f7232910ec8a60c517e1
Vagazzi <nikita19961104@gmail.com>
2022-09-19 21:05:23 +0300

    current knowledge of concurrency (will be fixed in the near future)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,26 @@
 package org.example;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Main {
-    public static void main(String[] args) {
-    Thread thread = new Thread(()->System.out.println("suc"));
-    thread.start();
+    public static void main(String[] args){
+
+        Sucktion sucktion = new Sucktion();
+
+        Thread thread = new Thread(sucktion::printSucktion);
+        Thread thread1 = new Thread(sucktion::printSucktion);
+
+        thread.start();
+        thread1.start();
+
+    }
+
+    public static class Sucktion{
+
+        int i = 1;
+        public synchronized void printSucktion(){
+            i*=2;
+            System.out.println("sucktion" + i);
+        }
     }
 }
